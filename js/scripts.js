79b6b2305329a5f32b0cddfe0500d7b3304f7338
jared <jaredspecjr@gmail.com>
2018-03-23 13:07:36 -0700

    add information to all columns and rows for appropriate response, add links for information about epicodus

--- js/scripts.js
+++ js/scripts.js
@@ -15,12 +15,28 @@ $(document).ready(function(){
       $(".information").hide();
       $(".rubyRails").show();
     } else if (frontend === "Yes" && backend === "No"){
+      $(".rubyRails").hide();
+      $(".javaAndroid").hide();
+      $(".reccomendation").hide();
+      $(".information").hide();
       $(".cssReact").show();
     } else if (android === "Yes"){
+      $(".cssReact").hide();
+      $(".rubyRails").hide();
+      $(".reccomendation").hide();
+      $(".information").hide();
       $(".javaAndroid").show();
     } else if (team && problemSolve === "Yes" && backend && frontend && android === "No"){
+      $(".cssReact").hide();
+      $(".javaAndroid").hide();
+      $(".rubyRails").hide();
+      $(".information").hide();
       $(".reccomendation").show();
     } else {
+      $(".cssReact").hide();
+      $(".javaAndroid").hide();
+      $(".reccomendation").hide();
+      $(".rubyRails").hide();
       $(".information").show();
     }
   });
