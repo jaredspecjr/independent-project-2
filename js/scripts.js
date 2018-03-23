8965fa63e6eef0ddb3c37c896728c372f33bd90c
jared <jaredspecjr@gmail.com>
2018-03-23 09:28:11 -0700

    add form and set submit event handler

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#form1").submit(function(){
+    var answer1 = $("#answer1").val();
+    var answer2 = $("#answer2").val();
+    var answer3 = $("#answer3").val();
+    var answer4 = $("#answer4").val();
+    var answer5 = $("#answer5").val();
+  });
+
+});
